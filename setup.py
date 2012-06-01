e15375790633e5943b30312dd68cd3ab36e4eb48
Kaz Sasa <kazssym@vx68k.org>
2012-06-01 20:19:31 +0900

    Added a Python package.
    --HG--
    branch : python

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from distutils.core import setup
 setup(name = 'prcs2hg',
       version = '1.0x',
       url = 'https://bitbucket.org/kazssym/prcs2hg/',
+      packages = ['prcs2hg'],
       scripts = ['scripts/prcs2hg'],
 )
